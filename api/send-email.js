c20d51a9b1caeaf80a4af1e396224ff88786151a
azwar-as <azwaranshari08@gmail.com>
2025-10-04 00:35:43 +0700

    modification send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -2,7 +2,7 @@ const nodemailer = require("nodemailer");
 
 export default async function handler(req, res) {
   // Tambahkan Header CORS
-  res.setHeader("Access-Control-Allow-Origin", "*"); // bisa juga spesifik ke domain kamu
+  res.setHeader("Access-Control-Allow-Origin", "https://azwar-as.github.io"); // domain kamu
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
