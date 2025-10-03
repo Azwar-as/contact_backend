33a0200e53cc84a0575fa4571d8c08173778af13
azwar-as <azwaranshari08@gmail.com>
2025-10-04 00:22:06 +0700

    modfication in api/send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -1,6 +1,16 @@
 const nodemailer = require("nodemailer");
 
 export default async function handler(req, res) {
+  // Tambahkan Header CORS
+  res.setHeader("Access-Control-Allow-Origin", "*"); // bisa juga spesifik ke domain kamu
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Kalau OPTIONS (preflight), langsung balikin 200
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method not allowed" });
   }
